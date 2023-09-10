01c0550452b718f88b56fff54e93182a825e78b3
Eelco Luurtsema <eelcoluurtsema@gmail.com>
2023-09-10 22:43:02 +0200

    HttpAPI: add user agent

--- src/Providers/HttpApi.php
+++ src/Providers/HttpApi.php
@@ -19,11 +19,16 @@ class HttpApi implements Scraper
 
     function get(string $url): Response
     {
-        return Http::get($url);
+        return Http::contentType('application/json')
+            ->acceptJson()
+            ->withUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36')
+            ->get($url);
     }
 
     function image(string $url): Response
     {
-        return Http::get($url);
+        return Http::withUserAgent(
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36'
+        )->get($url);
     }
 }

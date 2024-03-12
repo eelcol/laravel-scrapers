7f91cbbb81c24919a561558e90a757964444ec7e
Eelco <eelcoluurtsema@gmail.com>
2024-03-12 09:42:09 +0100

    HTTP API: keep cookies

--- src/Providers/HttpApi.php
+++ src/Providers/HttpApi.php
@@ -15,6 +15,8 @@ class HttpApi implements Scraper
 
     protected bool $remember_cookies = false;
 
+    protected array $cookies = [];
+
     public function instantiate(array $headers, bool $rememberCookies, bool $premium): self
     {
         $this->premium = $premium;
@@ -34,19 +36,23 @@ class HttpApi implements Scraper
             ->withUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36')
             ->get($url);
 
-        return ScrapeResponse::fromResponse($response);
+        return $this->processResponse($response);
     }
 
     public function post(string $url, array $data = []): ScrapeResponse
     {
+        $domainParts = parse_url($url);
+        $domain = $domainParts['host'];
+
         $response = Http::contentType('application/json')
             ->acceptJson()
             ->withHeaders($this->headers)
+            ->withCookies($this->prepareCookies(), $domain)
             ->withUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36')
             ->asForm()
             ->post($url, $data);
 
-        return ScrapeResponse::fromResponse($response);
+        return $this->processResponse($response);
     }
 
     public function image(string $url): ScrapeResponse
@@ -56,5 +62,35 @@ class HttpApi implements Scraper
         )->get($url);
 
         return ScrapeResponse::fromResponse($response);
+    }
+
+    protected function processResponse(Response $response)
+    {
+        $headers = $response->headers();
+
+        if ($this->remember_cookies && isset($headers['Set-Cookie'])) {
+            foreach ($headers['Set-Cookie'] as $c) {
+                $cookie = substr($c, 0, strpos($c, ";"));
+                if (!str_contains($cookie, "=")) {
+                    $this->cookies[] = ['name' => $cookie, 'value' => ""];
+                } else {
+                    $cookieName = substr($cookie, 0, strpos($cookie, "="));
+                    $cookieValue = substr($cookie, strpos($cookie, "=") + 1);
+                    $this->cookies[] = ['name' => $cookieName, 'value' => $cookieValue];
+                }
+            }
+        }
+
+        return ScrapeResponse::fromResponse($response);
+    }
+
+    protected function prepareCookies(): array
+    {
+        $cookies = [];
+        foreach ($this->cookies as $cookie) {
+            $cookies[$cookie['name']] = $cookie['value'];
+        }
+
+        return $cookies;
     }
 }
